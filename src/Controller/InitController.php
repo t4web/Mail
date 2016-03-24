8ec68144f481b11a01b5a98d9e0c3dd99e57283b
maxgu <gulturyan@t4web.com.ua>
2016-03-24 12:51:53 +0200

    Init Controller

--- src/Controller/InitController.php
+++ src/Controller/InitController.php
@@ -35,19 +35,12 @@ class InitController extends AbstractActionController
         $table->addColumn(new Ddl\Column\Char('subject', 250, true));
         $table->addColumn(new Ddl\Column\Text('body', null, true));
         $table->addColumn(new Ddl\Column\Text('allowed_vars', null, true));
-        $table->addColumn(new Ddl\Column\Datetime('dt_created', false, 'NOW()'));
-        $table->addColumn(new Ddl\Column\Datetime('dt_updated', false, 'NOW()'));
+        $table->addColumn(new Ddl\Column\Datetime('dt_created', false));
+        $table->addColumn(new Ddl\Column\Datetime('dt_updated', false));
         $table->addConstraint(new Ddl\Constraint\PrimaryKey('id'));
         $table->addConstraint(new Ddl\Constraint\UniqueKey('name'));
 
-        $sql = new Sql($this->dbAdapter);
-
-        try {
-            $this->dbAdapter->query($sql->buildSqlString($table), DbAdapter::QUERY_MODE_EXECUTE);
-        } catch (\Exception $e) {
-            // currently there are no db-independent way to check if table exists
-            // so we assume that table exists when we catch exception
-        }
+        $this->createTable($table);
 
 
         $table = new Ddl\CreateTable('mail_log');
@@ -59,16 +52,26 @@ class InitController extends AbstractActionController
         $table->addColumn(new Ddl\Column\Integer('template_id'));
         $table->addColumn(new Ddl\Column\Text('body', null, true));
         $table->addColumn(new Ddl\Column\Text('calculated_vars', null, true));
-        $table->addColumn(new Ddl\Column\Datetime('dt_created', false, 'NOW()'));
+        $table->addColumn(new Ddl\Column\Datetime('dt_created', false));
         $table->addConstraint(new Ddl\Constraint\PrimaryKey('id'));
 
+        $this->createTable($table);
+    }
+
+    private function createTable(Ddl\CreateTable $table)
+    {
         $sql = new Sql($this->dbAdapter);
 
         try {
             $this->dbAdapter->query($sql->buildSqlString($table), DbAdapter::QUERY_MODE_EXECUTE);
+        } catch (\PDOException $e) {
+            if (strpos($e->getMessage(), 'table or view already exists') === false) {
+                echo $e->getMessage() . PHP_EOL;
+                exit(1);
+            }
         } catch (\Exception $e) {
-            // currently there are no db-independent way to check if table exists
-            // so we assume that table exists when we catch exception
+            echo  $e->getMessage() . PHP_EOL;
+            exit(1);
         }
     }
 }
